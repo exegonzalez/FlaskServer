6b981abffd8af3e562e7990642205582e95fc88e
Exequiel <exe.gonzalez14@hotmail.com>
2020-02-13 22:10:39 -0300

    make it better

--- functionsDB/ConnectionDB.py
+++ functionsDB/ConnectionDB.py
@@ -1,7 +1,7 @@
 import psycopg2 as dbapi
 
 def abrirconexion():
-    con = dbapi.connect("dbname='ecommerceDB' user='postgres' host='localhost' password='gimnasiadeentrerios14'")
+    con = dbapi.connect("dbname='d6ufcnrlk9c7lb' user='iqgxcjpyhzimhk' host='ec2-52-203-160-194.compute-1.amazonaws.com' password='b961f23b0f9d5d31b9fa9bc8d9af26e8548ffb7453cfe568b13d9abd62b8d546'")
     cur = con.cursor()
     return cur,con
 
